187ffbe38897f1cd3aae29b2ab4b9b491fd67fae
Kirill Shakurov <kirill.shakurov@gmail.com>
2022-12-07 22:46:10 +0500

    На проверку

---
+++ ImageFeed/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Kirill on 06.12.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let ypWhite: UIColor = UIColor(named:"YP White")!
+    static let ypBlack: UIColor = UIColor(named:"YP Black")!
+    static let ypGray: UIColor = UIColor(named: "YP Gray")!
+}
